652874455d6d0ceb34cc55033f498875559b2a37
aYaremchuk <jaremchuk.andrij@gmail.com>
2019-06-23 12:39:50 +0300

    Add app pages stubs

--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'app/index.html', {})
+
+def category(request, pk):
+    return render(request, 'app/category.html', {})
+
+def courses_list(request):
+    return render(request, 'app/courses_list.html', {})
+
+def course(request, pk):
+    return render(request, 'app/course.html', {})
+
+def study(request, pk):
+    return render(request, 'app/study.html', {})
+
+def study_unit(request, pk, unit_pk):
+    return render(request, 'app/study_unit.html', {})
+
+def user(request):
+    return render(request, 'app/user.html', {})
+
+def user_study(request):
+    return render(request, 'app/user_study.html', {})
+
+def user_settings(request):
+    return render(request, 'app/user_settings.html', {})
