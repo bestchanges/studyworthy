d01197e97708757a90da1657eb3ddf27dd44e942
aYaremchuk <jaremchuk.andrij@gmail.com>
2019-06-25 21:01:39 +0300

    Add simple specs to check response code for pages

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 * [Canban board](https://github.com/bestchanges/studyworthy/projects/1)
 * [Unassigned Issues](https://github.com/bestchanges/studyworthy/issues?q=is%3Aopen+is%3Aissue+no%3Aassignee)
 * [Wiki](https://github.com/bestchanges/studyworthy/wiki)
+
+### Basic system specs
+* Django 2.2
+
+### Configuration
+* pip install -r requirements.txt
+
+### Run tests
+* ./manage.py test
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,43 @@
+import unittest
+from django.test import Client
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_courses_response(self):
+        response = self.client.get('/courses/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_course_response(self):
+        response = self.client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_response(self):
+        response = self.client.get('/category/1/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_study_response(self):
+        response = self.client.get('/study/1/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_study_unit_response(self):
+        response = self.client.get('/study/1/1')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_user_response(self):
+        response = self.client.get('/user/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_user_study_response(self):
+        response = self.client.get('/user/study')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_user_settings_response(self):
+        response = self.client.get('/user/settings')
+        self.assertEqual(response.status_code, 200)
