145baa92473916d48090e43852fb8e155f16b38d
efedorov-dart <49433890+efedorov-dart@users.noreply.github.com>
2019-06-26 13:34:09 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('category/<int:pk>/', views.category, name='category'),
 
     path('study/<int:pk>/', views.study, name='study'),
-    path('study/<int:pk>/<int:unit_pk>', views.study_unit, name='study_unit'),
+    path('study1/<int:pk>/<int:unit_pk>', views.study_unit, name='study_unit'),
 
     path('user/', views.user, name='user'),
     path('user/study', views.user_study, name='user_study'),
