1172e3a2c6b1802148dc6ff9bb5acbf6299b7925
Egor Fedorov <egor.fedorov@dataart.com>
2019-06-26 18:29:13 +0300

    rename application serve static files

--- study/tests.py
+++ study/tests.py
@@ -5,37 +5,37 @@ from django.urls import reverse
 class SimpleTest(TestCase):
 
     def test_home_response(self):
-        response = self.client.get(reverse('app:index'))
+        response = self.client.get(reverse('study:index'))
         self.assertEqual(response.status_code, 200)
 
     def test_courses_response(self):
-        response = self.client.get(reverse('app:courses'))
+        response = self.client.get(reverse('study:courses'))
         self.assertEqual(response.status_code, 200)
 
     def test_course_response(self):
-        response = self.client.get(reverse('app:course', kwargs={'pk': 1}))
+        response = self.client.get(reverse('study:course', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 200)
 
     def test_category_response(self):
-        response = self.client.get(reverse('app:category', kwargs={'pk': 1}))
+        response = self.client.get(reverse('study:category', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 200)
 
     def test_study_response(self):
-        response = self.client.get(reverse('app:study', kwargs={'pk': 1}))
+        response = self.client.get(reverse('study:study', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 200)
 
     def test_study_unit_response(self):
-        response = self.client.get(reverse('app:study_unit', kwargs={'pk': 1, 'unit_pk': 1}))
+        response = self.client.get(reverse('study:study_unit', kwargs={'pk': 1, 'unit_pk': 1}))
         self.assertEqual(response.status_code, 200)
 
     def test_user_response(self):
-        response = self.client.get(reverse('app:user'))
+        response = self.client.get(reverse('study:user'))
         self.assertEqual(response.status_code, 200)
 
     def test_user_study_response(self):
-        response = self.client.get(reverse('app:user_study'))
+        response = self.client.get(reverse('study:user_study'))
         self.assertEqual(response.status_code, 200)
 
     def test_user_settings_response(self):
-        response = self.client.get(reverse('app:user_settings'))
+        response = self.client.get(reverse('study:user_settings'))
         self.assertEqual(response.status_code, 200)
--- studyworthy/settings.py
+++ studyworthy/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'study',
     'manage',
 ]
 
@@ -120,5 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 TEST_RUNNER = "redgreenunittest.django.runner.RedGreenDiscoverRunner"
