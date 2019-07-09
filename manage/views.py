7ae4caee43b0eec69320d582bfa73347dc125d0d
Andrew Yaremchuk <jaremchuk.andrij@gmail.com>
2019-07-09 10:25:29 +0300

    Add manage page stubs and simple tests (#17)

--- manage/views.py
+++ manage/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def courses(request):
+    return render(request, 'app/courses.html', {})
+
+def start_course(request, pk):
+    return render(request, 'app/start_course.html', {})
+
+def study(request):
+    return render(request, 'app/study.html', {})
+
+def study_manage(request, pk):
+    return render(request, 'app/study_manage.html', {})
