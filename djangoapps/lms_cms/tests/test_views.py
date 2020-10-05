17fda942841a9df76fc7e4673c49741f165069e9
Egor Fedorov <egor.fedorov@gmail.com>
2020-10-05 19:33:26 +0300

    * more tests

--- djangoapps/lms_cms/tests/test_views.py
+++ djangoapps/lms_cms/tests/test_views.py
@@ -1,6 +1,10 @@
+from urllib.parse import urlencode
+
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
+
+from djangoapps.lms.models.lms_models import Course
 
 
 class TestViews(TestCase):
@@ -22,3 +26,30 @@ class TestViews(TestCase):
         client: Client = self.client
         response = client.get(reverse('lms_cms:student_kabinet'))
         self.assertEqual(response.status_code, 302)
+
+    def test_flow_access(self):
+        """
+        Flow accesible for:
+         * ANY it's participant (Teacher, Student, Admin)
+         * super admin
+        Flow is not accessible:
+         * anonymous user -> shall redirect
+         * user has not been assigned to the course - 403
+         * user assigned to course but participating in OTHER flow - 403
+        """
+        client: Client = self.client
+        course = Course.objects.create()
+        flow = course.create_flow()
+
+        # 1. Anonymous user should redirect to login
+        flow_view_url = reverse('lms_cms:flow-view', args=[flow.id])
+        expected_params = {
+            'next': flow_view_url
+        }
+        expected_redirect_url = f'{reverse("login")}?{urlencode(expected_params)}'
+
+        response = client.get(flow_view_url)
+        self.assertRedirects(response, expected_redirect_url)
+
+        # self.fail("Need more cases")
+        # 2. Logged user
